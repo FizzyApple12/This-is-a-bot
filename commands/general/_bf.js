30261d730ca3fc75d901175a395be8dd70037d2b
FizzyApple12 <davideroeder@gmail.com>
2020-07-02 17:09:40 -0400

    fixed the brainfuck command

--- commands/general/_bf.js
+++ commands/general/_bf.js
@@ -1,94 +1,60 @@
 const message = require("../../programLogic/events/message");
-
+ 
 const Discord = module.require("discord.js");
 const config = module.require('../../config.json');
 const messageSanitizer = module.require('../../messageSanitizer.js');
+ 
+const runBrainfuckCode = async (code, callback) => {
+    var mem_arr = [];
+    var data_ptr = 0;
+    var infiniteCheck = 0;
+    var output = "";
+    var jsSourceCode = '';
 
-// modified from: https://github.com/treker7/brainfuck-interpreter/blob/master/brainfuckInterpreter.js
-// I was too lazy to write my own.
-
-const MEM_SIZE = 30000;
-const mem_arr = [];
-var data_ptr = 0;
-var output = "";
-
-var init = function() {
-    data_ptr = 0;
-    output = "";
-    mem_arr.length = MEM_SIZE;
-    mem_arr.fill(0);
-}
-
-var compile = (bfSourceCode) => {
-    var jsSourceCode = '(function(ptr, arr) {';
-
-    const codeLength = bfSourceCode.length;
-    for (var i = 0; i < codeLength; i++) {
-        const currBfInstr = bfSourceCode.charAt(i);
+    for (var i = 0; i < code.length; i++) {
+        const currBfInstr = code.charAt(i);
 
         switch (currBfInstr) {
             case '>':
-                jsSourceCode += '++ptr;';
+                jsSourceCode += 'data_ptr++;';
                 break;
             case '<':
-                jsSourceCode += '--ptr;';
+                jsSourceCode += 'data_ptr--;';
                 break;
             case '+':
-                jsSourceCode += 'arr[ptr] += 1;';
+                jsSourceCode += 'if (!mem_arr[data_ptr]) mem_arr[data_ptr] = 0; mem_arr[data_ptr] += 1;';
                 break;
             case '-':
-                jsSourceCode += 'arr[ptr] -= 1;';
+                jsSourceCode += 'if (!mem_arr[data_ptr]) mem_arr[data_ptr] = 0; mem_arr[data_ptr] -= 1;';
                 break;
             case '.':
-                jsSourceCode += 'output += String.fromCharCode(arr[ptr]);';
+                jsSourceCode += 'output += String.fromCharCode(mem_arr[data_ptr]);';
                 break;
             case ',':
-                // jsSourceCode += 'arr[ptr] = readline.question().charCodeAt(0);';
+                // jsSourceCode += 'mem_arr[data_ptr] = readline.question().charCodeAt(0);';
                 // disable input as this has no way of being done as of now
                 break;
             case '[':
-                jsSourceCode += 'while(arr[ptr]) {';
+                jsSourceCode += 'while(mem_arr[data_ptr]) {';
                 break;
             case ']':
-                jsSourceCode += '}';
+                jsSourceCode += `infiniteCheck++; if (infiniteCheck >= 1000) break; } infiniteCheck = 0;`;
                 break;
             default:
         }
     }
 
-    jsSourceCode += '}(data_ptr, mem_arr));'
+    eval(jsSourceCode)
 
-    return jsSourceCode;
-};
-
+    callback(output);
+}
 
 exports.run = async function (bot, msg, args) {
-    return messageSanitizer.sendChannel(msg, "brainfuck has been disabled while I work in a better implementation");
-    init();
-    var jsCode = compile(args[1]);
-    var timedOut = true;
-    setTimeout(() => {
-        if (timedOut) {
-            throw new Error("ExecutionError: Script took too long to execute.");
-        }
-    }, 5000);
-    Aexec(jsCode, () => {
-        timedOut = false;
-
-        if (output.includes("@")) {
-            messageSanitizer.sendChannel(msg, 'Ok, while I applaud the effort for creating a brainfuck program that pings, I am going to have to disallow that. Amazing work though.')
-            return msg.react(bot.emojis.get('728284525926547466'));
-        }
-
-        messageSanitizer.sendChannel(msg, output);
+    runBrainfuckCode(args[1], output => {
+        messageSanitizer.sendChannel(msg, " " + output);
     });
 }
-
-var Aexec = async (code, callback) => {
-    eval(code);
-    callback();
-}
-
+ 
 exports.info = {
     name: 'bf',
     usage: 'bf "[code]"',
